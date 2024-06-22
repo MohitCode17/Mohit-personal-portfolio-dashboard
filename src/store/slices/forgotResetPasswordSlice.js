4b4f3c51f2735f958d1713ace8adcd4149964ed5
MohitCode17 <mohit.codes17@gmail.com>
2024-06-22 17:42:56 +0530

    add forgot and reset password slice

---
+++ src/store/slices/forgotResetPasswordSlice.js
@@ -0,0 +1,115 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const forgotResetPasswordSlice = createSlice({
+  name: "forgotPassword",
+  initialState: {
+    loading: false,
+    error: null,
+    message: null,
+  },
+
+  reducers: {
+    forgotPasswordRequest(state, action) {
+      state.loading = true;
+      state.error = null;
+      state.message = null;
+    },
+    forgotPasswordSuccess(state, action) {
+      state.loading = false;
+      state.error = null;
+      state.message = action.payload;
+    },
+    forgotPasswordFailed(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+      state.message = null;
+    },
+    resetPasswordRequest(state, action) {
+      state.loading = true;
+      state.error = null;
+      state.message = null;
+    },
+    resetPasswordSuccess(state, action) {
+      state.loading = false;
+      state.error = null;
+      state.message = action.payload;
+    },
+    resetPasswordFailed(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+      state.message = null;
+    },
+    clearAllErrors(state, action) {
+      state.error = null;
+      state = state;
+    },
+  },
+});
+
+// FORGOT PASSWORD REQUEST
+export const forgotPassword = (email) => async (dispatch) => {
+  dispatch(forgotResetPasswordSlice.actions.forgotPasswordRequest());
+
+  try {
+    const { data } = await axios.post(
+      "http://localhost:8000/api/v1/user/password/forgot",
+      { email },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(data);
+
+    dispatch(
+      forgotResetPasswordSlice.actions.forgotPasswordSuccess(data.message)
+    );
+  } catch (error) {
+    console.log(error);
+    dispatch(
+      forgotResetPasswordSlice.actions.forgotPasswordFailed(
+        error.response.data.message
+      )
+    );
+  }
+};
+
+// RESET PASSWORD REQUEST
+export const resetPassword =
+  (token, password, confirmPassword) => async (dispatch) => {
+    dispatch(forgotResetPasswordSlice.actions.resetPasswordRequest());
+
+    try {
+      const { data } = await axios.put(
+        `http://localhost:8000/api/v1/user/password/reset/${token}`,
+        { password, confirmPassword },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(data);
+      dispatch(
+        forgotResetPasswordSlice.actions.resetPasswordSuccess(data.message)
+      );
+    } catch (error) {
+      console.log(error);
+      dispatch(
+        forgotResetPasswordSlice.actions.resetPasswordFailed(
+          error.response.data.message
+        )
+      );
+    }
+  };
+
+export const clearAllForgotResetPassErrors = () => (dispatch) => {
+  dispatch(forgotResetPasswordSlice.actions.clearAllErrors());
+};
+
+export default forgotResetPasswordSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import forgotResetPasswordReducer from "./slices/forgotResetPasswordSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    forgotPassword: forgotResetPasswordReducer,
   },
 });
