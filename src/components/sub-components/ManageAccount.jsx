6ad5afb2fa791b410e2bca49f920541ec6d6975b
MohitCode17 <mohit.codes17@gmail.com>
2024-06-23 21:01:12 +0530

    add user profile ui

--- src/components/sub-components/ManageAccount.jsx
+++ src/components/sub-components/ManageAccount.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Profile from "./Profile";
+import UpdateProfile from "./UpdateProfile";
+import UpdatePassword from "./UpdatePassword";
+
 const ManageAccount = () => {
-  return <div>ManageAccount</div>;
+  const [activeComponent, setActiveComponent] = useState("Profile");
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        <div className="mx-auto grid w-full max-w-6xl gap-2">
+          <h1 className="text-3xl font-semibold">Settings</h1>
+        </div>
+        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+          <nav className="grid gap-4 text-sm text-muted-foreground">
+            <Link
+              className={
+                activeComponent === "Profile"
+                  ? "font-semibold text-primary"
+                  : ""
+              }
+              onClick={() => setActiveComponent("Profile")}
+            >
+              Profile
+            </Link>
+
+            <Link
+              className={
+                activeComponent === "Update Profile"
+                  ? "font-semibold text-primary"
+                  : ""
+              }
+              onClick={() => setActiveComponent("Update Profile")}
+            >
+              Update Profile
+            </Link>
+
+            <Link
+              className={
+                activeComponent === "Update Password"
+                  ? "font-semibold text-primary"
+                  : ""
+              }
+              onClick={() => setActiveComponent("Update Password")}
+            >
+              Update Password
+            </Link>
+          </nav>
+          <div className="grid gap-6">
+            {(() => {
+              switch (activeComponent) {
+                case "Profile":
+                  return <Profile />;
+                  break;
+
+                case "Update Profile":
+                  return <UpdateProfile />;
+                  break;
+
+                case "Update Password":
+                  return <UpdatePassword />;
+                  break;
+              }
+            })()}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default ManageAccount;
---
+++ src/components/sub-components/Profile.jsx
@@ -0,0 +1,87 @@
+import { useSelector } from "react-redux";
+import { Label } from "../ui/label";
+import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+
+const Profile = () => {
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
+
+  return (
+    <>
+      <div className="w-full h-full">
+        <div>
+          <div className="grid w-[100%] gap-6">
+            <div className="grid gap-2">
+              <h1 className="text-3xl font-bold">Profile</h1>
+            </div>
+            <div className="grid gap-4">
+              <div className="flex items-start lg:justify-between lg:items-center flex-col lg:flex-row gap-5">
+                <div className="grid gap-2 w-full sm:w-72">
+                  <Label>Profile Image</Label>
+                  <img
+                    src={user && user.avatar && user.avatar.url}
+                    alt="avatar"
+                    className="w-full h-auto sm:w-72 sm:h-72 rounded-2xl object-cover"
+                  />
+                </div>
+                <div className="grid gap-2 w-full sm:w-72">
+                  <Label>Resume</Label>
+                  <Link
+                    to={user && user.resume && user.resume.url}
+                    target="_blank"
+                  >
+                    <img
+                      src={user && user.resume && user.resume.url}
+                      alt="avatar"
+                      className="w-full h-auto sm:w-72 sm:h-72 rounded-2xl object-cover object-top"
+                    />
+                  </Link>
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label>Full Name</Label>
+                <Input type="text" defaultValue={user.fullName} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>Email</Label>
+                <Input type="email" defaultValue={user.email} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>Phone</Label>
+                <Input type="text" defaultValue={user.phone} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>About Me</Label>
+                <Textarea defaultValue={user.aboutMe} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>Github URL</Label>
+                <Input type="text" defaultValue={user.githubUrl} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>LinkedIn URL</Label>
+                <Input type="text" defaultValue={user.linkedinUrl} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>Instagram URL</Label>
+                <Input type="text" defaultValue={user.instagramUrl} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>Twitter(X) URL</Label>
+                <Input type="text" defaultValue={user.twitterUrl} disabled />
+              </div>
+              <div className="grid gap-2">
+                <Label>Facebook URL</Label>
+                <Input type="text" defaultValue={user.facebookUrl} disabled />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
