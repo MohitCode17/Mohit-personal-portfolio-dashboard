0f87a45104b82a4a7010f5c44cede8f695b15cfb
MohitCode17 <mohit.codes17@gmail.com>
2024-06-24 17:34:59 +0530

    add update password functionality

--- src/components/sub-components/UpdatePassword.jsx
+++ src/components/sub-components/UpdatePassword.jsx
@@ -1,5 +1,94 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import LoadingButton from "./LoadingButton";
+import { useEffect, useState } from "react";
+import {
+  clearAllUserErrors,
+  getUser,
+  resetProfile,
+  updatePassword,
+} from "@/store/slices/userSlice";
+import { toast } from "react-toastify";
+
 const UpdatePassword = () => {
-  return <div>UpdatePassword</div>;
+  const { loading, isUpdated, error, message } = useSelector(
+    (state) => state.user
+  );
+
+  const dispatch = useDispatch();
+
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleUpdatePassword = () => {
+    dispatch(updatePassword(currentPassword, newPassword, confirmPassword));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearAllUserErrors());
+    }
+
+    if (isUpdated) {
+      dispatch(getUser());
+      dispatch(resetProfile());
+    }
+
+    if (message) {
+      toast.success(message);
+    }
+  }, [dispatch, error, loading, isUpdated]);
+
+  return (
+    <>
+      <div className="w-full h-full">
+        <div>
+          <div className="grid w-[100%] gap-6">
+            <div className="grid gap-2">
+              <h1 className="text-3xl font-bold">Update Password</h1>
+            </div>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label>Current Password</Label>
+                <Input
+                  type="password"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label>New Password</Label>
+                <Input
+                  type="password"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label>Confirm New Password</Label>
+                <Input
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              {!loading ? (
+                <Button onClick={handleUpdatePassword} className="w-full">
+                  Update Password
+                </Button>
+              ) : (
+                <LoadingButton content={"Updating"} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UpdatePassword;
